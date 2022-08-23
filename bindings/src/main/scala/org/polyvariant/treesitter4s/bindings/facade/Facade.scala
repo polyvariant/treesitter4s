a27fdff69828930c3d7cb80486b059f4a8307443
Jakub Kozłowski <kubukoz@gmail.com>
2022-08-23 04:41:53 +0200

    Revert "get rid of uint32?"
    This reverts commit 8549b256233a287fc2e22199d994e7f5f221e715.

--- bindings/src/main/scala/org/polyvariant/treesitter4s/bindings/facade/Facade.scala
+++ bindings/src/main/scala/org/polyvariant/treesitter4s/bindings/facade/Facade.scala
@@ -49,7 +49,7 @@ private[bindings] object Facade {
             parserPointer,
             null /* old tree */,
             sourceBytes,
-            sourceBytes.length,
+            new treesitter4s.bindings.Uint32_t(sourceBytes.length),
             toNative.encoding(encoding),
           )
         }
