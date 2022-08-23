181eb31912c12b9e92c179801eb674e31bfa1db5
Jakub Kozłowski <kubukoz@gmail.com>
2022-08-23 05:39:33 +0200

    Add dev note

--- bindings/src/main/scala/org/polyvariant/treesitter4s/bindings/facade/Facade.scala
+++ bindings/src/main/scala/org/polyvariant/treesitter4s/bindings/facade/Facade.scala
@@ -59,6 +59,8 @@ private[bindings] object Facade {
         try {
           val tree = mkTree(parserPointer)
           try fromNative.tree(ts, tree)
+          // note: removing this delete fixes tests on Dotty.
+          // there's probably some memory we're not copying that we should
           finally ts.ts_tree_delete(tree)
         } finally ts.ts_parser_delete(parserPointer)
 
