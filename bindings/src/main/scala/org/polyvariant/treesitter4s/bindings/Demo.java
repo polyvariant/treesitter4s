4b08b9f7af534ef80cb6f1a71cafb78c216a68f8
Jakub Kozłowski <kubukoz@gmail.com>
2022-08-27 22:57:55 +0200

    back to utf8

--- bindings/src/main/scala/org/polyvariant/treesitter4s/bindings/Demo.java
+++ bindings/src/main/scala/org/polyvariant/treesitter4s/bindings/Demo.java
@@ -12,13 +12,13 @@ public class Demo {
 		Parser p = ts.ts_parser_new();
 		ts.ts_parser_set_language(p, scala);
 
-		byte[] bytes = "class Hello".getBytes(java.nio.charset.StandardCharsets.UTF_16);
-		Tree t = ts.ts_parser_parse_string_encoding(p, null, bytes, bytes.length, 1/* utf16 */);
+		byte[] bytes = "class Hello\n\n".getBytes(java.nio.charset.StandardCharsets.UTF_8);
+		Tree t = ts.ts_parser_parse_string_encoding(p, null, bytes, bytes.length, 0/* utf8 */);
 
 		Node.ByValue n = ts.ts_tree_root_node(t);
 
 		System.out.println(ts.ts_node_child_count(n));
-		// System.out.println(ts.ts_node_string(n));
+		System.out.println(ts.ts_node_string(n));
 		// ts.ts_tree_delete(t);
 		// ts.ts_parser_delete(p);
 
