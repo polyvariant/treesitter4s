73935a54cedd39f04ab04db47793a7e06e999789
Jakub Kozłowski <kubukoz@gmail.com>
2022-10-13 21:47:59 +0200

    Revert "test"
    This reverts commit e31196e31fc58ba9afbf1d83bea99dbd5a2d2f06.

--- bindings/src/main/scala/org/polyvariant/treesitter4s/bindings/TreeSitterLanguages.scala
+++ bindings/src/main/scala/org/polyvariant/treesitter4s/bindings/TreeSitterLanguages.scala
@@ -32,8 +32,6 @@ object TreeSitterLanguages {
       copyLibFromCL("c++abi.1", cl)
       val p2 = copyLibFromCL("c++.1.0", cl)
 
-      com.sun.jna.NativeLibrary.getInstance(p2.toString())
-
       loadLibFromCL(p2)
     }
   }
