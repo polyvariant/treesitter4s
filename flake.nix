d1d5df4d5eca3847b90f245779dcc76ee1eed605
Jakub Kozłowski <kubukoz@gmail.com>
2022-10-31 03:34:32 +0100

    Remove unused stuff from binaries maker

--- flake.nix
+++ flake.nix
@@ -19,13 +19,6 @@
               install_name_tool -id $2.dylib $out/lib/$2.dylib
             }
 
-            function copyGrammar() {
-              # 1 - grammar derivation path, 2 - target lib name
-              cp "$1/parser" "$out/lib/$2.dylib"
-              chmod +w "$out/lib/$2.dylib"
-              install_name_tool -id $2.dylib $out/lib/$2.dylib
-            }
-
             function renameLib() {
               # 1 - library path, 2 - library name, 3 - what depends on this
               install_name_tool -change $1/lib/$2.dylib @loader_path/$2.dylib $out/lib/$3.dylib
@@ -47,16 +40,6 @@
             copyLib ${pkgs.libcxx} "libc++.1.0"
             renameLib ${pkgs.libcxxabi} libc++abi.1 libc++.1.0
             copyLib ${pkgs.libcxxabi} "libc++abi.1"
-
-            # scala grammar
-            copyGrammar ${pkgs.tree-sitter-grammars.tree-sitter-scala} libtree-sitter-scala
-            renameLib ${pkgs.libcxxabi} libc++abi.1 libtree-sitter-scala
-            renameLib ${pkgs.libcxx} libc++.1.0 libtree-sitter-scala
-
-            # python grammar
-            copyGrammar ${pkgs.tree-sitter-grammars.tree-sitter-python} libtree-sitter-python
-            renameLib ${pkgs.libcxxabi} libc++abi.1 libtree-sitter-python
-            renameLib ${pkgs.libcxx} libc++.1.0 libtree-sitter-python
           '';
         };
     in
