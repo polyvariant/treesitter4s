ab65c8576a893b6800c7622ebe173f5afa8de57e
Jakub Kozłowski <kubukoz@gmail.com>
2022-10-31 03:33:31 +0100

    this works too

--- update-libs.sh
+++ update-libs.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-mkdir -p "modules/language-$1/.jvm/src/main/resources/"
-
-RESULT=$(nix build ".#$1-grammar-all" --no-link --print-out-paths --print-build-logs)
+RESULT=$(nix build ".#grammars" --no-link --print-out-paths --print-build-logs)
 # copy, flattening links
 cp --no-preserve=mode,ownership -Lr "$RESULT"/* .
